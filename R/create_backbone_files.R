b17af55b3d4d823e8c7dfc06414d4f7df7aeb743
Lucas Moraes <moraessaur@gmail.com>
2021-07-22 15:34:44 -0300

    up

---
+++ R/create_backbone_files.R
@@ -0,0 +1,18 @@
+#' Create backbone file system
+#'
+#' This function creates a template of folders which I am used to,
+#' when starting new projects
+#'
+#' @return 5 folders, in the current directory
+#' @export
+#'
+#' @examples
+#' create_backbone_files()
+create_backbone_files <- function(){
+  a <- dir.create("scripts")
+  b <- dir.create("img")
+  c <- dir.create("notebooks")
+  d <- dir.create("data_raw")
+  e <- dir.create("data_processed")
+  return(list(a,b))
+}
