e9dba7535e510aed834df8d681b307932c3e2866
Lucas Moraes <moraessaur@gmail.com>
2021-08-25 18:31:39 -0300

    adding boilerplate

--- R/check_install_packages.R
+++ R/check_install_packages.R
@@ -1,3 +1,14 @@
+#' check_install_packages
+#' check if each package in vector is installed, if not, installs it
+#' @param x vector of package names to be checked an installed
+#' @param y cran mirror
+#'
+#' @return install packages if not detected
+#' @export
+#'
+#' @examples
+#'
+#' check_install_packages(c("lubridate","devtools"))
 check_install_packages <-
   function(x,y="https://vps.fmvz.usp.br/CRAN/"){
     for (k in x){
